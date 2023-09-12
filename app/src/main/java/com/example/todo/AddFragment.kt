555537216fc79a217ea1e798fec53dfef699cb28
Hasan <hasanboronov0523@gmail.com>
2023-09-12 19:38:04 +0500

    3

--- app/src/main/java/com/example/todo/AddFragment.kt
+++ app/src/main/java/com/example/todo/AddFragment.kt
@@ -1,6 +1,7 @@
 package com.example.todo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,10 @@ import androidx.core.widget.addTextChangedListener
 import com.example.todo.database.AppDatabase
 import com.example.todo.database.Task
 import com.example.todo.databinding.FragmentAddBinding
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.Calendar
+import java.util.Date
 
 class AddFragment : Fragment() {
 
@@ -20,15 +25,37 @@ class AddFragment : Fragment() {
 
         binding.addTimePicker.setIs24HourView(true)
         binding.addAddBtn.isEnabled = false
-
+        var isToday = true
         binding.addEdittext.addTextChangedListener {
             binding.addAddBtn.isEnabled = binding.addEdittext.text.toString().isNotEmpty()
         }
+        binding.addDatepicker.minDate = Date().time
+        binding.addDatepicker.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->
+            val today = LocalDate.now()
+            if (dayOfMonth == today.dayOfMonth && monthOfYear+1 == today.monthValue && year == today.year){
+                val now = LocalTime.now()
+                binding.addTimePicker.hour = now.hour
+                binding.addTimePicker.minute = now.minute
+                isToday = true
+            }else{
+                isToday = false
+            }
+        }
+        binding.addTimePicker.setOnTimeChangedListener { view, hourOfDay, minute ->
+            val now = LocalTime.now()
+            if (isToday){
+                if (hourOfDay < now.hour || (hourOfDay == now.hour && minute < now.minute)){
+                    binding.addTimePicker.hour = now.hour
+                    binding.addTimePicker.minute = now.minute
+                }
+            }
+        }
 
         binding.addAddBtn.setOnClickListener {
+
             val day :Int = binding.addDatepicker.dayOfMonth
             val month:Int = binding.addDatepicker.month + 1
-            val year:Int = binding.addDatepicker.dayOfMonth
+            val year:Int = binding.addDatepicker.year
 
             val hour = binding.addTimePicker.hour
             val min = binding.addTimePicker.minute
